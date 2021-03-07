28ad7ee1a83468dbb746b498d7d3d24bdaeae646
Shekhinah Memmel <she@khinah.xyz>
2021-03-07 12:59:33 +0100

    feat: add panopticon stats request test cases

--- src/server.rs
+++ src/server.rs
@@ -48,7 +48,7 @@ pub async fn run_server(
                             log::error!("{:?}", err);
                             process::exit(-1);
                         }
-                        HttpResponse::Ok().finish().await
+                        HttpResponse::Ok()
                     }
                 },
             ),
@@ -70,4 +70,41 @@ pub async fn run_server(
     };
 
     Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::model;
+    use actix_web::http::{header, Method, StatusCode};
+    use actix_web::{test, web, App, HttpMessage, HttpRequest, HttpResponse, HttpServer};
+    #[actix_rt::test]
+    async fn test_requests() {
+        let mut app = test::init_service(App::new().route(
+            "/report-usage-stats/push",
+            web::put().to(
+                move |req: web::HttpRequest, stats: web::Json<model::StatsReport>| async {
+                    HttpResponse::Ok().await
+                },
+            ),
+        ))
+        .await;
+
+        let mut test_payloads = HashMap::new();
+        test_payloads.insert("v0.33.6", include_str!("../tests/report-v0.33.6.json"));
+        test_payloads.insert("v0.99.2", include_str!("../tests/report-v0.99.2.json"));
+        test_payloads.insert("v0.99.4", include_str!("../tests/report-v0.99.4.json"));
+        test_payloads.insert("v1.28.0", include_str!("../tests/report-v1.28.0.json"));
+
+        for (version, payload) in test_payloads {
+            let req = test::TestRequest::put()
+            .uri("/report-usage-stats/push")
+            .insert_header(header::ContentType::json())
+            .set_payload(payload)
+            .to_request();
+
+            let resp = test::call_service(&mut app, req).await;
+            assert_eq!(resp.status(), StatusCode::OK, "testing panopticon {} report-usage-stats request", version);
+        }
+    }
 }
